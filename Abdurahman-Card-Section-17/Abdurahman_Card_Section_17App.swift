246f9b6d3b23e3d45a9a2731d7e491724fe52b8e
Abdurahman AYDIN <hariysu@gmail.com>
2024-01-20 16:08:20 +0300

    Initial Commit

---
+++ Abdurahman-Card-Section-17/Abdurahman_Card_Section_17App.swift
@@ -0,0 +1,17 @@
+//
+//  Abdurahman_Card_Section_17App.swift
+//  Abdurahman-Card-Section-17
+//
+//  Created by Abdurahman on 20.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Abdurahman_Card_Section_17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
